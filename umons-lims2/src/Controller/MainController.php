6ed5aeff61eb21f338347d7a7aacdf5f633a714d
ArnaudPalgen <arnaud.palgen@student.umons.ac.be>
2021-04-07 20:54:15 +0200

    base html v1

---
+++ umons-lims2/src/Controller/MainController.php
@@ -0,0 +1,30 @@
+<?php
+// src/Controller/MainController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MainController extends AbstractController{
+    /**
+     * @Route("/lucky/number")
+     */
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+
+        /**
+     * @Route("/login")
+     */
+    public function login(): Response
+    {
+        return $this ->render('login.html.twig');
+    }
+    
+}
