8d53bc8a114b902feaaf5dbf50da5f976d2ce22b
pierre503 <pierre503@live.be>
2021-04-09 19:35:51 +0200

    aujout jqery + début page product

--- umons-lims2/src/Controller/MainController.php
+++ umons-lims2/src/Controller/MainController.php
@@ -19,13 +19,30 @@ class MainController extends AbstractController{
         );
     }
 
-        /**
-     * @Route("/login")
-     */
+    /**
+    * @Route("/login")
+    */
     public function login(): Response
     {
         $users=array("Jean", "Jeanne", "Marc", "Jerome", "Paul", "Thomas", "Jaqueline", "Jilbert", "Jean-Jacques");
         return $this ->render('login.html.twig', ['users' => $users]);
+    }
+	
+	/**
+    * @Route("/product")
+    */
+    public function product(): Response
+    {
+        $product=array(
+			array("used" => True, "cas" => "331-39-5", "name" => "Caffeic acid", "location" => "A 3", "concentration" => "80%", "open" => True),
+			array("used" => True, "cas" => "50-78-2", "name" => "Aspirin", "location" => "B 6", "concentration" => "80%", "open" => True),
+			array("used" => False, "cas" => "331-39-5", "name" => "Caffeic acid", "location" => "A 3", "concentration" => "60%", "open" => True),
+			array("used" => False, "cas" => "57-27-2", "name" => "Morphine", "location" => "D 5", "concentration" => "90%", "open" => False),
+			array("used" => False, "cas" => "100-52-7", "name" => "Benzaldehyde", "location" => "A 3", "concentration" => "80%", "open" => True)
+			);
+		
+		
+        return $this ->render('product.html.twig', ['product' => $product]);
     }
     
 }
