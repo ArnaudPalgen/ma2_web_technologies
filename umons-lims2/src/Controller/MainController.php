dfef5e4900b64bbe1deb8b40235ede6654f04c87
pierre503 <pierre503@live.be>
2021-04-12 17:43:16 +0200

    fin frontend "produit"

--- umons-lims2/src/Controller/MainController.php
+++ umons-lims2/src/Controller/MainController.php
@@ -91,11 +91,11 @@ class MainController extends AbstractController{
     public function product(): Response
     {
         $product=array(
-			array("used" => True, "cas" => "331-39-5", "name" => "Caffeic acid", "location" => "A 3", "concentration" => "80%", "open" => True),
-			array("used" => True, "cas" => "50-78-2", "name" => "Aspirin", "location" => "B 6", "concentration" => "80%", "open" => True),
-			array("used" => False, "cas" => "331-39-5", "name" => "Caffeic acid", "location" => "A 3", "concentration" => "60%", "open" => True),
-			array("used" => False, "cas" => "57-27-2", "name" => "Morphine", "location" => "D 5", "concentration" => "90%", "open" => False),
-			array("used" => False, "cas" => "100-52-7", "name" => "Benzaldehyde", "location" => "A 3", "concentration" => "80%", "open" => True)
+			array("used" => True, "cas" => "331-39-5", "name" => "Caffeic acid", "location" => "A 3", "concentration" => "80%", "used"=>"Pierre Zielinski", "open" => True),
+			array("used" => True, "cas" => "50-78-2", "name" => "Aspirin", "location" => "B 6", "concentration" => "80%", "used"=>"Serge NSHIMIYIMANA","open" => True),
+			array("used" => False, "cas" => "331-39-5", "name" => "Caffeic acid", "location" => "A 3", "concentration" => "60%", "used"=>Null,"open" => True),
+			array("used" => False, "cas" => "57-27-2", "name" => "Morphine", "location" => "D 5", "concentration" => "90%", "used"=>Null,"open" => False),
+			array("used" => False, "cas" => "100-52-7", "name" => "Benzaldehyde", "location" => "A 3", "concentration" => "80%", "used"=>Null,"open" => True)
 			);
 
 
