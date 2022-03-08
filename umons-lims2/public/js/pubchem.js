76be7aa716b1e65caeae4aacb4ee2a8c42f3b36d
pierre503 <pierre503@live.be>
2022-03-08 17:23:06 +0100

    [CHG] correction of cas check and add substance to product check

--- umons-lims2/public/js/pubchem.js
+++ umons-lims2/public/js/pubchem.js
@@ -9,17 +9,7 @@ class PubChem {
 
     isCasValid(cas) {
         let match_ = cas.match(/([0-9]{2,7})-([0-9]{2})-[0-9]/);
-        if(!match_) {
-            return false
-        }
-        let digits = (match_[1] + match_[2]).split('').reverse();
-        let sum = 0;
-
-        for (let i = 0; i < digits.length; i++) {
-            sum += (i + 1) * parseInt(digits[i]);
-        }
-
-        return sum % 10;
+        return match_
     }
 
     async autocompleteCAS(input) {
@@ -27,9 +17,12 @@ class PubChem {
             const resp = await this.api.get(`/autocomplete/compound/${input}`);
             return resp.data.dictionary_terms.compound;
         } catch (e) {
-            console.error('PubChem call failed !')
-            console.log(e);
-            return [];
+			try {
+				const resp = await this.api.get(`/autocomplete/substance/${input}`);
+				return resp.data.dictionary_terms.compound;
+			} catch (e) {
+				return [];
+			}
         }
 
     }
@@ -47,20 +40,28 @@ class PubChem {
 
 
         } catch (e) {
-            console.error('PubChem call failed !')
-            console.log(e);
+            try {
+				const resp =  await this.api.get(`/pug/substance/name/${ncas}/cids/JSON`);
+                return resp.data.InformationList.Information[0].CID[0];
+			} catch (e) {
+				console.error('PubChem call failed !')
+				console.log(e);
+			}
         }
     };
 
     async findProductName(cid) {
 
         try {
-
             const resp = await this.api.get(`/pug/compound/cid/${cid}/synonyms/JSON`);
             return resp.data.InformationList.Information[0].Synonym[0];
         } catch (e) {
-            console.error('PubChem call failed !')
-            console.log(e);
+			try {
+				const resp = await this.api.get(`/pug/substance/cid/${cid}/synonyms/JSON`);
+				return resp.data.InformationList.Information[0].Synonym[0];
+			} catch (e) {
+				console.log(e);
+			}
         }
     }
 
@@ -78,7 +79,24 @@ class PubChem {
 
         } catch (e) {
             if(e.response.status === 404) {
-                return [];
+				try {
+					const resp = await this.api.get(`/pug_view/data/substance/${cid}/JSON?heading=Chemical+Safety`);
+					return resp.data.Record.Section[0].Information[0].Value.StringWithMarkup[0].Markup.map(e=>{
+						let s =  e.URL.split("/");
+						return {
+							symbol: e.URL,
+							code: s[s.length -1].split('.')[0],
+							text:e.Extra
+						}
+					});
+				} catch (e) {
+					if(e.response.status === 404) {
+						return [];
+					} else{
+						console.error('PubChem call failed !')
+						console.log(e);
+					}
+				}
             } else{
                 console.error('PubChem call failed !')
                 console.log(e);
